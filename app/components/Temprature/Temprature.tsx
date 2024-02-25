a1111cae3f63498b20f4aace3dd86f90cf046142
Abhinav <abhinavprajapait2001@gmail.com>
2024-02-25 21:22:32 +0530

    feat: add card to show live temprature

---
+++ app/components/Temprature/Temprature.tsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { useState } from 'react'
+import { useGlobalContext } from '@/app/context/globalContext'
+import { kelvinToCelsius } from '@/app/utils/misc'
+import { drizzleIcon, rain, snow, clearSky, cloudy, navigation } from '@/app/utils/Icons'
+
+const Temprature = () => {
+  const { forecast } = useGlobalContext()
+  // console.log(forecast)
+  const { main, timezone, name, weather } = forecast
+
+  if (!forecast || !weather) return <div>Loading... </div>
+
+  const getIcon = () => {
+    switch (weather[0].main) {
+      case 'Drizzle':
+        return drizzleIcon
+      case 'Rain':
+        return rain
+      case 'Snow':
+        return snow
+      case 'Clear':
+        return clearSky
+      case 'Clouds':
+        return cloudy
+      default:
+        return clearSky
+    }
+  }
+
+  const temp = kelvinToCelsius(main?.temp);
+  const minTemp = kelvinToCelsius(main?.temp_min);
+  const maxTemp = kelvinToCelsius(main?.temp_max);
+
+  const [localTime, setLocalTime] = useState<string>("")
+  const [currentDay, setCurrentDay] = useState<string>("")
+
+  const { main: weatherMain, description } = weather[0];
+
+  console.log(weather)
+
+  return (
+    <div className=' px-4 pt-6 pb-5 border rounded-lg flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none'>
+      <p className="flex justify-between items-center ">
+        <span className="font-medium">{currentDay}</span>
+        <span className="font-medium">{localTime}</span>
+      </p>
+      <p className="pt-2 font-bold flex gap-1">
+        <span className="">{name}</span>
+        <span className="">{navigation}</span>
+      </p>
+      <p className="py-10 text-9xl font-bold self-center">{temp}°</p>
+      <div className="">
+        <div className="">
+          <span>{getIcon()}</span>
+          <p className='pt-2 capitalize text-lg font-medium'>{description}</p>
+        </div>
+        <p>
+          <span>Low: {minTemp}°</span>
+          <span>High: {maxTemp}°</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Temprature
