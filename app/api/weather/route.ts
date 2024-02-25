8ebe790d623a89b2dd9f6d67983c2ee921b638ab
Abhinav <abhinavprajapait2001@gmail.com>
2024-02-25 20:39:25 +0530

    fix: change url to get weather info

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -3,14 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
     try {
         const apiKey = process.env.OPEN_WEATHER_API_KEY
-        const lat = 40
-        const lon = -3
-        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        const lat = 20.1
+        const lon = -3.4
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        console.log(url)
         const response = await axios.get(url)
-
-        console.log(response.data)
+        // console.log(response.data)
         return NextResponse.json(response.data)
-        // console.log(url)
 
     } catch (error) {
         console.log("error fetching data")
