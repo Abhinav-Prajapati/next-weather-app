dd0da9c730ec0b0a35e33981afb0d3d83dff21fc
Abhinav <abhinavprajapait2001@gmail.com>
2024-02-27 02:48:05 +0530

    Update globalContext.js to fetch air quality data and provides it to application

--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useContext, createContext, useEffect } from "react"
+import React, { useContext, createContext, useEffect, use } from "react"
 import { useState } from "react";
 import axios from "axios";
 
@@ -8,20 +8,37 @@ const GlobalContextUpdate = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
   const [forecast, setForecast] = useState({})
+  const [airQuality, setAirQuality] = useState({})
+
+  // Fetch air quality data from the apiroutes
+  const fetchAirQuality = async () => {
+    try {
+      const response = await axios.get('api/pollution') // TODO: change name pullution ot air quality
+      console.log(response.data)
+      setAirQuality(response.data)
+    } catch (error) {
+      console.log("Error fetching air quality data from apiroutes: ", error.message)
+    }
+  }
+
+  // Fetch forecast data from the apiroutes
   const fetchForecast = async () => {
     try {
       const response = await axios.get('api/weather')
+      // console.log(response.data)
       setForecast(response.data)
     } catch (error) {
       console.log("Error fetching forecast data: ", error.message)
     }
   }
+
   useEffect(() => {
-    fetchForecast()
+    fetchForecast();
+    fetchAirQuality();
   }, [])
 
   return (
-    <GlobalContext.Provider value={{ forecast }}>
+    <GlobalContext.Provider value={{ forecast, airQuality }}>
       <GlobalContextUpdate.Provider>
         {children}
       </GlobalContextUpdate.Provider>
